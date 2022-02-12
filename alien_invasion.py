e0f10d5861ebf2d0ac088b4c8af524815bcfa2f6
Galileo <469860236@qq.com>
2022-02-12 16:30:00 +0800

    new one

---
+++ alien_invasion.py
@@ -0,0 +1,57 @@
+import pygame
+from pygame.sprite import Group
+from alien import Alien
+import sys
+from settings import Settings
+from game_stats import GameStats
+from button import Button
+from ship import Ship
+from scoreboard import Scoreboard
+import game_functions as gf
+
+
+def run_game():
+    # 初始化
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # 设置背景色
+    # bg_color = (230, 230, 230)
+
+    # 创建Play按钮
+    play_button = Button(ai_settings, screen, "Play")
+
+    # 创建一个用于储存游戏统计数据的实例,并创建记分牌
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+    # 创建一个外星人
+    alien = Alien(ai_settings, screen)
+
+    # 创建一艘飞船
+    ship = Ship(ai_settings, screen)
+
+    # 创建一个储存子弹的编组
+    bullets = Group()
+
+    # 创建一个外星人编组
+    aliens = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    # 游戏主循环
+    while True:
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
+        if stats.game_active:
+            ship.uptate()
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            # 查看屏幕中子弹数量
+            # print(len(bullets))
+
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
+
+
+run_game()
